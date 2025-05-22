800daea5be7c608b79d6169d88439baa05a459b9
rayhan <148326856+ryonw23@users.noreply.github.com>
2025-05-22 21:26:39 +0800

    Create bubblesort.py

---
+++ Bubblesort/bubblesort.py
@@ -0,0 +1,22 @@
+def bubble_sort(arr):
+    swapped = True
+    length = len(arr) - 1
+    while(swapped):
+        swapped = False
+        for i in range(length):
+            if(arr[i]>arr[i+1]):
+                store = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = store
+                swapped = True
+        length -= 1
+
+input_string = input("Enter array (comma or space separated): ")
+
+arr = [int(x) for x in input_string.replace(',', ' ').split()]
+
+print("Original array:", arr)
+
+bubble_sort(arr)
+
+print("Sorted array:", arr)
