f7a9b26303c7175d7826e4b1495fe0a0758bc6d8
rayhan <148326856+ryonw23@users.noreply.github.com>
2025-05-22 22:36:34 +0800

    Create selectionsort.py

---
+++ Selectionsort/selectionsort.py
@@ -0,0 +1,28 @@
+def min_pos(arr,start,end):
+    min = arr[start]
+    pos = start
+    for i in range(start,end+1):
+        if(arr[i]<min):
+            min = arr[i]
+            pos = i
+    return pos
+
+
+def selection_sort(arr):
+    length = len(arr)-1
+    for i in range(length):
+        pos_min = min_pos(arr,i,length)
+        store = arr[pos_min]
+        arr[pos_min] = arr[i]
+        arr[i] = store
+        print(arr)
+    
+input_string = input("Enter array (comma or space separated): ")
+
+arr = [int(x) for x in input_string.replace(',', ' ').split()]
+
+print("Original array:", arr)
+
+selection_sort(arr)
+
+print("Sorted array:", arr)
