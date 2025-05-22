717f51c1b2d2ff136fbae33f7df1cf2d0f5ae86a
rayhan <148326856+ryonw23@users.noreply.github.com>
2025-05-22 21:50:01 +0800

    Create insertionsort.js

---
+++ Insertionsort/insertionsort.js
@@ -0,0 +1,34 @@
+//Run in Node.js
+function insertionSort(arr)
+{
+    let length = arr.length;
+    for(let i=1; i<length; i++)
+    {
+        let ins = arr[i];
+        index = i-1;
+        while(index>=0 && ins<arr[index])
+        {
+            arr[index+1] = arr[index];
+            index--;
+        }
+        arr[index+1] = ins;
+    }
+}
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readline.question('Enter array (comma or space separated): ', (input) => 
+{
+    let arr = input.split(/[\s,]+/).map(Number);
+
+    console.log('Original array:', arr);
+
+    insertionSort(arr);
+
+    console.log('Sorted array:', arr);
+
+    readline.close();
+});
