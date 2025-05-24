d6239f577eae36330cb6f21abf0093a4e5a01e15
rayhan <148326856+ryonw23@users.noreply.github.com>
2025-05-24 22:01:51 +0800

    add radix sort to countingsort.py

--- Countingsort/countingsort.py
+++ Countingsort/countingsort.py
@@ -1,3 +1,5 @@
+import math
+
 def find_max(arr):
     max = arr[0]    
     for i in range(len(arr)):
@@ -21,7 +23,35 @@ def counting_sort(arr):
         pos += count[i]
     return sorted
 
+def radix_sort(arr):
+    digits = len(str(find_max(arr)))
+    print("Count of digits in longest number: ", digits)
+
+    for i in range(digits):
+        dig_arr = []
+        queues = []
+        for j in range(10):
+            queues.append([])
+        
+        for j in range(len(arr)):
+            digit = math.floor((arr[j]/(10 ** i)) % 10)
+            dig_arr.append(digit)
+            queues[digit].append(arr[j])
+        
+        sorted_digs = counting_sort(dig_arr)
+        print("Queues of digits:", queues)
+        print("Array of digits in", i, "th place:", dig_arr, "Sorted:", sorted_digs)
+
+        index = 0
+        for j in range(len(sorted_digs)):
+            digit = sorted_digs[j]
+            arr[index] = queues[digit].pop(0)
+            index += 1
+    return arr
+
+
 input_string = input("Enter array (comma or space separated): ")
 arr = [int(x) for x in input_string.replace(","," ").split()]
 print("Original array: ", arr)
-print("Sorted array: ", counting_sort(arr))
+radix_sort(arr)
+print("Sorted array: ", arr)
